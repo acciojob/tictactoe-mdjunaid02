7c0fca634c8a7956f37f9b2b157935e9a9910bf5
mdjunaid02 <156643108+mdjunaid02@users.noreply.github.com>
2025-11-11 11:25:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
-//your JS code here. If required.
-document.addEventListener(){
-document.getElementById('submit').onclick=function(){
-	document.querySelector('.input-section').style.display='none';
-	document.querySelector('.game-section').style.display='block';
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('submit').onclick = function() {
+    document.querySelector('.input-section').style.display = 'none';
+    document.querySelector('.game-section').style.display = 'block';
 
+    const player1 = document.getElementById("player1").value;
+    const player2 = document.getElementById("player2").value;
 
-const player1=document.getElementById("player1").value;
-const player2=document.getElementById("player2").value;
+    let turn = 'X';
+    let currPlayer = player1;
 
-let turn='X';
-let currPlayer=player1;
+    const cells = document.querySelectorAll(".cell");
 
-const cell=document.querySelectorAll(".cell");
+    document.querySelector(".message").textContent = player1 + " you're up";
 
-document.querySelector(".message").textContent=player1+" you're up";
-
-cell.forEach(cell => {
-	cell.onclick=function(){
-		if(cell.textContent===''){
-			cell.textContent=turn;
-			if(turn==='X'){
-				turn='O';
-				currPlayer=player2;
-			}else{
-				turn='X';
-				currPlayer=player1;
-			}
-			document.querySelector(".message").textContent=currPlayer+" you're up.";
-		}
-	}
+    cells.forEach(cell => {
+      cell.onclick = function() {
+        if(cell.textContent === ''){
+          cell.textContent = turn;
+          if(turn === 'X'){
+            turn = 'O';
+            currPlayer = player2;
+          } else {
+            turn = 'X';
+            currPlayer = player1;
+          }
+          document.querySelector(".message").textContent = currPlayer + " you're up.";
+        }
+      };
+    });
+  }
 });
-}
-};
